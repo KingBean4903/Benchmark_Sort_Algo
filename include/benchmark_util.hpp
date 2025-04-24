9225343db94453d2eb3edd1720a3c6726c252ae4
KingBean4903 <davidkanyango065@gmail.com>
2025-04-24 20:25:38 +0300

    Tested basic std::sort

---
+++ include/benchmark_util.hpp
@@ -0,0 +1,43 @@
+#ifndef BENCHMARK_UTIL_HPP
+#define BENCHMARK_UTIL_HPP
+
+#include <vector>
+#include <random>
+#include <chrono>
+
+namespace benchmark {
+
+	using Clock = std::chrono::high_resolution_clock;
+
+	template<typename T>
+	std::vector<T> generate_random_data(size_t size, T min = 0,
+										T max = 100000)
+	{
+			std::vector<T> data(size);
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			std::uniform_int_distribution<T> dist(min, max);
+			
+			for (auto& val : data)
+			{
+					val = dist(gen);
+			}
+			return data;
+	}
+
+	template<typename Func, typename... Args>
+	double time_function(Func&& func, Args&&... args) { 
+		
+		auto start = Clock::now();
+		func(std::forward<Args>(args)...);
+		auto end = Clock::now();
+		std::chrono::duration<double, std::milli> elapsed = 
+				end - start;
+		
+		return elapsed.count();
+	}
+	
+		
+}
+
+#endif
